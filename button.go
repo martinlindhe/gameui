0cfaa2e8e64bf6e231f15ff6353ceda398f917ec
Martin Lindhe <martin-commit@ubique.se>
2016-09-30 22:54:31 +0200

    ui rework

--- button.go
+++ button.go
@@ -2,7 +2,10 @@ package ui
 
 import (
 	"image"
+	"image/color"
 	"image/draw"
+
+	"github.com/martinlindhe/farm/game"
 )
 
 // Button ...
@@ -26,11 +29,14 @@ func (btn *Button) SetImage(img *image.RGBA) {
 
 // Draw ...
 func (btn *Button) Draw() (*image.RGBA, error) {
-	// XXX show the button on screen
 
+	// XXX reuse internal image buffer
 	rect := image.Rect(0, 0, btn.Width, btn.Height)
 	im := image.NewRGBA(rect)
-	// XXX
+
+	// draw button outline
+	game.DrawRect(im, &rect, color.White)
+
 	if btn.image != nil {
 		// XXX draw image centered in btn.
 		// XXX warn if image is bigger than btn. then auto shrink?
--- button_test.go
+++ button_test.go
@@ -3,10 +3,32 @@ package ui
 import (
 	"image"
 	"image/color"
+	"log"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
+
+func TestButtonOnly(t *testing.T) {
+	w, h := 30, 8
+	btn := NewButton(w, h)
+
+	im, err := btn.Draw()
+	assert.Equal(t, nil, err)
+	testCompareRender(t, []string{
+		"999999999999999999999999999999",
+		"9                            9",
+		"9                            9",
+		"9                            9",
+		"9                            9",
+		"9                            9",
+		"9                            9",
+		"999999999999999999999999999999",
+	}, renderAsText(im))
+
+	//ui := New(w, h)
+	// ui.AddComponent(btn)
+}
 
 // for testing
 func renderAsText(img *image.RGBA) []string {
@@ -30,24 +52,29 @@ func scale(valueIn, baseMin, baseMax, limitMin, limitMax float64) float64 {
 // turn col brightness into ascii
 func colToText(col color.Color) string {
 	vals := []string{
-		" ", ".", ",", "3", "4", "5", "6", "7", "8", "9",
+		" ", ".", ",", "+", "o", "5", "6", "O", "0", "#",
 	}
 	r, g, b, _ := col.RGBA()
-	avg := (r + g + b/3)
+	avg := (r + g + b) / 3
 	// XXX include alpha by using it as pct of value c
-	n := int(scale(float64(avg), 0, 255, 0, 9))
+	n := int(scale(float64(avg), 0, 0xffff, 0, 9))
+	if n > len(vals) {
+		log.Fatal("XXX n too long ", n, len(vals))
+	}
 	return vals[n]
 }
 
-func TestButtonOnly(t *testing.T) {
-	w, h := 30, 10
-	btn := NewButton(w, h)
-
-	im, err := btn.Draw()
-	assert.Equal(t, nil, err)
-	txt := renderAsText(im)
-	assert.Equal(t, "xxx", txt)
-
-	//ui := New(w, h)
-	// ui.AddComponent(btn)
+func testCompareRender(t *testing.T, expected, got []string) {
+	if len(expected) != len(got) {
+		t.Error("expected", len(expected), "lines,got", len(got))
+	}
+	for i, ex := range expected {
+		if i >= len(got) {
+			t.Error("line", i+1, "expected", ex, "GOT NOTHING")
+			continue
+		}
+		if ex != got[i] {
+			t.Error("line", i+1, "expected", ex, "got", got[i])
+		}
+	}
 }
